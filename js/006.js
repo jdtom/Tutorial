789ab42799436b1e6369481d2d539a1bb3e10c49
jdtom <jdtom.info@gmail.com>
2016-03-24 23:23:30 +0800

    change css and js
    js to 900 and added a css class cc

--- js/006.js
+++ js/006.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
     paper.setup(document.getElementById('mainCanvas'));
         var c;
         var j=3;
-        for(var x=25; x<800; x+=80) {
-            for(var y=25; y<800; y+=80) {
+        for(var x=25; x<900; x+=80) {
+            for(var y=25; y<900; y+=80) {
             c = Shape.Circle(x, y, 30);
                 //alert(j);
                 if(j == 3){
